ac04090757eaa65b27d649b604998f1fc0a03e8e
Napigo <napigo.hometech@gmail.com>
2022-09-08 17:57:01 +0800

    Update Build Metadata for UAT environment
    -

--- src/build-banner/BuildBanner.tsx
+++ src/build-banner/BuildBanner.tsx
@@ -7,12 +7,19 @@ export const BuildBanner: React.FC = () => {
       <Box
         bgGradient={"linear(to-r, brandAlpha.800, brandAlpha.200)"}
         display={"flex"}
+        alignItems="center"
         justifyContent="center"
         padding={"1px"}
         paddingY="5px"
         fontWeight={"bold"}
+        gap={2}
       >
-        <Text>UAT Environment Build</Text>
+        <Text>
+          UAT Environment Build, Build ID: [{import.meta.env.VITE_BUILD_ID}]
+        </Text>
+        <Text fontSize={"sm"} fontWeight="light">
+          powered by {import.meta.env.VITE_POWERED_BY}
+        </Text>
       </Box>
     );
   }
