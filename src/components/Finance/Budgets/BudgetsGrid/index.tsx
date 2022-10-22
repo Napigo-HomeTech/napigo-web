053fa8b2e65f182468be82411a7e8724e7a5e299
Napigo <napigo.hometech@gmail.com>
2022-10-23 01:25:22 +0800

    Created Pagination Component & Fixed Grid System pagination function loader for Budgets

--- src/components/Finance/Budgets/BudgetsGrid/index.tsx
+++ src/components/Finance/Budgets/BudgetsGrid/index.tsx
@@ -1,11 +1,56 @@
-import { FinanceApis, useGetBudgetsQuery } from "@/lib/API/finance-apis";
-import React, { useState } from "react";
+import { Pagination } from "@/elements";
+import { BudgetItem, useGetBudgetsQuery } from "@/lib/API/finance-apis";
+import { SimpleGrid, Box, HStack, Heading } from "@chakra-ui/react";
+import { uniqueId } from "lodash";
+import React, { Fragment, useEffect, useState } from "react";
 
 const BudgetsGrid: React.FC = () => {
     const [page, setPage] = useState<number>(1);
-    const { data, isError, isLoading } = useGetBudgetsQuery(1, { skip: true });
 
-    return <h1>hello There</h1>;
+    const [total, setTotal] = useState<number>(0);
+    const { data, isLoading, isFetching } = useGetBudgetsQuery(page);
+
+    useEffect(() => {
+        if (!isLoading && data?.counts) {
+            setTotal(data.counts);
+        }
+    }, [isLoading]);
+
+    return (
+        <Fragment>
+            <HStack width={"100%"} height="50px" justifyContent={"flex-end"}>
+                <Pagination
+                    countPerPage={20}
+                    totalCounts={total}
+                    onPageChange={(page: number) => {
+                        setPage(page);
+                    }}
+                />
+            </HStack>
+
+            <SimpleGrid columns={5} spacing={5} minChildWidth="250px" overflow="scroll" maxHeight={"500px"}>
+                {!isLoading && !isFetching && data && data.budgets && data.budgets.length > 0 && (
+                    <>
+                        {data.budgets.map((budget: BudgetItem) => {
+                            return (
+                                <Box
+                                    key={uniqueId()}
+                                    bg="gray.700"
+                                    rounded={"md"}
+                                    height="80px"
+                                    flexDirection={"row"}
+                                    display="flex"
+                                    justifyContent={"center"}
+                                    alignItems="center"
+                                >
+                                    <Heading size={"sm"}>{budget.revision}</Heading>
+                                </Box>
+                            );
+                        })}
+                    </>
+                )}
+            </SimpleGrid>
+        </Fragment>
+    );
 };
-
 export default BudgetsGrid;
--- src/pages/Finance/Budgeting/BudgetListing/index.tsx
+++ src/pages/Finance/Budgeting/BudgetListing/index.tsx
@@ -6,7 +6,7 @@ export const BudgetListingPage: React.FC = () => {
     return (
         <Fragment>
             <Stack gap={4} flexDirection="column" p={"20px"}>
-                <Box width="100%" height="200px" borderWidth={1} rounded={"md"} />
+                <Box width="100%" height="100px" borderWidth={1} rounded={"md"} />
                 <BudgetsGrid />
             </Stack>
         </Fragment>
