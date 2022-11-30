9b1f72aff7f5b6142ae71845d36e2c795d96368a
Napigo <napigo.hometech@gmail.com>
2022-11-30 11:46:02 +0800

    refactor the DatafieldRow component with ItemRemove and its dialog styling

--- src/components/Finance/Plan/Forms/ItemsTable/DatafieldRow.tsx
+++ src/components/Finance/Plan/Forms/ItemsTable/DatafieldRow.tsx
@@ -1,38 +1,15 @@
-import React, { useRef } from "react";
-import {
-  Tr,
-  Td,
-  IconButton,
-  useDisclosure,
-  AlertDialog,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogCloseButton,
-  AlertDialogBody,
-  AlertDialogFooter,
-  Button,
-} from "@chakra-ui/react";
-import { FaTrashAlt as RemoveItemIcon } from "react-icons/fa";
+import React from "react";
+import { Tr, Td } from "@chakra-ui/react";
+
 import { ItemNameDatafield } from "./ItemNameDatafield";
 import { ItemAmountDatafield } from "./ItemAmountDatafield";
-import { fixtures } from "@/constant/datasets/fixtures";
-import { useDispatch } from "react-redux";
-import { PlanformActions } from "@/lib/Redux/planform.reducer";
+
+import { ItemRemove } from "./ItemRemove";
 
 type DatafieldRowProps = {
   itemId: string;
 };
 export const DatafieldRow: React.FC<DatafieldRowProps> = ({ itemId }) => {
-  const dispatch = useDispatch();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = useRef<HTMLButtonElement>(null);
-
-  const onConfirmDelete = () => {
-    dispatch(PlanformActions.removePlanItem(itemId));
-    onClose();
-  };
-
   return (
     <Tr overflow="visible">
       <Td paddingLeft={1}>
@@ -42,55 +19,7 @@ export const DatafieldRow: React.FC<DatafieldRowProps> = ({ itemId }) => {
         <ItemAmountDatafield itemId={itemId} />
       </Td>
       <Td paddingRight={1} isNumeric>
-        <IconButton
-          colorScheme={"base"}
-          aria-label="Remove item"
-          onClick={() => onOpen()}
-          color="red.500"
-          icon={<RemoveItemIcon />}
-        />
-        <AlertDialog
-          motionPreset="slideInBottom"
-          leastDestructiveRef={cancelRef}
-          onClose={onClose}
-          isOpen={isOpen}
-          isCentered
-        >
-          <AlertDialogOverlay />
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              {
-                fixtures.financeStrings[
-                  "finance.planform.tableform.item.dialog.deleteitem.header"
-                ]
-              }
-            </AlertDialogHeader>
-            <AlertDialogCloseButton />
-            <AlertDialogBody>
-              {
-                fixtures.financeStrings[
-                  "finance.planform.tableform.item.dialog.deleteitem.message"
-                ]
-              }
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                {
-                  fixtures.financeStrings[
-                    "finance.planform.tableform.item.dialog.deleteitem.button-cancel"
-                  ]
-                }
-              </Button>
-              <Button colorScheme="red" ml={3} onClick={onConfirmDelete}>
-                {
-                  fixtures.financeStrings[
-                    "finance.planform.tableform.item.dialog.deleteitem.button-delete"
-                  ]
-                }
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <ItemRemove itemId={itemId} />
       </Td>
     </Tr>
   );
---
+++ src/components/Finance/Plan/Forms/ItemsTable/ItemRemove.tsx
@@ -0,0 +1,132 @@
+import React, { useCallback, useRef } from "react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Heading,
+  IconButton,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FaTrashAlt as RemoveItemIcon } from "react-icons/fa";
+import { fixtures } from "@/constant/datasets/fixtures";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/lib/Redux/store";
+import { find } from "lodash";
+import { PlanformActions } from "@/lib/Redux/planform.reducer";
+
+type ItemRemoveProps = {
+  itemId: string;
+};
+export const ItemRemove: React.FC<ItemRemoveProps> = ({ itemId }) => {
+  const dispatch = useDispatch();
+  const itemName = useSelector((state: RootState) => {
+    const items = state.planformStore.items;
+    const target = find(items, { item_id: itemId });
+    return target?.name;
+  });
+
+  const onConfirmDelete = useCallback(() => {
+    dispatch(PlanformActions.removePlanItem(itemId));
+  }, [dispatch, itemId]);
+
+  return (
+    <MemoizedItemRemove
+      itemName={itemName as string}
+      onConfirmDelete={onConfirmDelete}
+    />
+  );
+};
+
+type MemoItemRemoveProps = {
+  itemName: string;
+  onConfirmDelete: () => void;
+};
+const MemoItemRemove = ({ itemName, onConfirmDelete }: MemoItemRemoveProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+  return (
+    <>
+      <IconButton
+        colorScheme={"base"}
+        aria-label="Remove item"
+        onClick={() => onOpen()}
+        color="red.500"
+        icon={<RemoveItemIcon />}
+      />
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+          <AlertDialogHeader color="text-hard">
+            {
+              fixtures.financeStrings[
+                "finance.planform.tableform.item.dialog.deleteitem.header"
+              ]
+            }
+          </AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            <Heading size="sm" marginBottom={0}>
+              {itemName}
+            </Heading>
+            <Text color="text-hard">
+              {
+                fixtures.financeStrings[
+                  "finance.planform.tableform.item.dialog.deleteitem.message"
+                ]
+              }
+            </Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              {
+                fixtures.financeStrings[
+                  "finance.planform.tableform.item.dialog.deleteitem.button-cancel"
+                ]
+              }
+            </Button>
+            <Button
+              colorScheme="red"
+              ml={3}
+              onClick={() => {
+                onConfirmDelete();
+                onClose();
+              }}
+            >
+              {
+                fixtures.financeStrings[
+                  "finance.planform.tableform.item.dialog.deleteitem.button-delete"
+                ]
+              }
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+};
+
+/**
+ *
+ * @param prev
+ * @param next
+ * @returns
+ */
+const compareProps = (prev: MemoItemRemoveProps, next: MemoItemRemoveProps) => {
+  if (prev.itemName !== next.itemName) {
+    return false;
+  }
+  return true;
+};
+const MemoizedItemRemove = React.memo(MemoItemRemove, compareProps);
